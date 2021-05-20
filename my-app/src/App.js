8f6a4bdefab29d86ab3e5771776b90ff63a3a3f6
daryllast <daryl.last621@gmail.com>
2021-05-19 23:08:04 -0400

    attempt1 navigation bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from "./components/Navbar/Navbar";
 import './App.css';
 import TodoList from './components/TodoList';
+import App2 from './App2';
 
 function App() {
   return (
-    <div className='todo-app'>
+    <div className="container">
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/todolist" exact component={TodoList} />
+          <Route path="/app2" exact component={App2} />
+        </Switch>
+      </Router>
+      <div className='todo-app'>
       <TodoList />
+      </div>
+
     </div>
   );
 }
---
+++ my-app/src/components/Navbar/MenuList.js
@@ -0,0 +1,16 @@
+export const MenuList = [
+    {
+      title: "Home(Current)",
+      url: "/",
+    },
+    {
+      title: "To Do List",
+      url: "/todolist",
+    },
+    {
+      title: "Contact Form",
+      url: "/contactform",
+    },
+   
+  ];
+  
